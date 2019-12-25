4b426a320d52f85cc92bc5156a981e7fc3b87344
aleksandar.fs@gmail.com <aleksandar.fs@gmail.com>
2019-12-25 22:31:02 +0200

    Corrected unit testing

--- medxc-persistence-jpa/src/test/java/com/dxc/medxc/persistence/jpa/repository/JpaPatientRepositoryTest.java
+++ medxc-persistence-jpa/src/test/java/com/dxc/medxc/persistence/jpa/repository/JpaPatientRepositoryTest.java
@@ -3,64 +3,61 @@
  */
 package com.dxc.medxc.persistence.jpa.repository;
 
-import java.sql.Timestamp;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import javax.persistence.EntityManager;
+import java.util.stream.Collectors;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
+import com.dxc.medxc.commons.store.model.Store;
+import com.dxc.medxc.persistence.jpa.entities.JpaRecord;
+import com.dxc.medxc.persistence.jpa.transaction.TransactionManagerImpl;
+import com.dxc.medxc.persistence.transaction.TransactionManager;
 import org.junit.Test;
 import org.unitils.UnitilsJUnit4;
 import org.unitils.dbunit.annotation.DataSet;
-
-import com.dxc.medxc.persistence.jpa.entities.JpaAppointment;
-import com.dxc.medxc.persistence.jpa.entities.JpaRecord;
-import com.dxc.medxc.persistence.model.Appointment;
 import com.dxc.medxc.persistence.model.Record;
 import com.dxc.medxc.persistence.repository.PatientRepository;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.unitils.reflectionassert.ReflectionAssert.assertPropertyLenientEquals;
 
 /**
  * @author astefanov2
  */
 public class JpaPatientRepositoryTest extends UnitilsJUnit4 {
     private static final String PIN = "9812148221";
-
+    private static final String INVALID_PIN = "0000000000";
     private static final EntityManagerFactory entityManagerFactory = Persistence
             .createEntityManagerFactory("medxc-persistence-jpa");
-
+    final TransactionManager trxManager = new TransactionManagerImpl(entityManagerFactory);
 
 
     /**
      *
      */
-    @SuppressWarnings("nls")
     @Test
-    @DataSet({ "Clear.xml"})
-    public void testGetRecordsById() {
-        final EntityManager entityManager = entityManagerFactory.createEntityManager();
-        final PatientRepository patientRepo = new JpaPatientRepository(entityManager);
+    @DataSet({ "Clear.xml","RecordsDataInput.xml"})
+    public void testGetRecordsByIdWithExistingPatient() {
+        final List<JpaRecord> recs =  this.trxManager.execute((final Store store) ->{
+            final PatientRepository rep = store.find(PatientRepository.class);
+               return  rep.getAllRecordsById(PIN).stream().map(r -> new JpaRecord(r.getAppointment(),r.getAnamnesis(),r.getObservation(),r.getTherapy())).collect(Collectors.toList());
+            });
 
-        final List<Record> records = new ArrayList<>() {
-            {
-                add(new JpaRecord(new JpaAppointment(), "Amnesis", "B", "C"));
-                add(new JpaRecord(new JpaAppointment(), "Amnesis", "B", "C"));
-            }
-        };
-        assertTrue(true);
-/*
-        try {
-            final List<Record> recs = patientRepo.getAllRecordsById(PIN);
-            assertEquals(records.size(), recs.size());
-            assertEquals(records, recs);
-        } finally {
-            entityManager.close();
-        }*/
+        assertPropertyLenientEquals("id", Arrays.asList(1,2,3),recs);
     }
+    /**
+     *
+     */
+    @Test
+    @DataSet({ "Clear.xml","RecordsDataInput.xml"})
+    public void testGetRecordsByIdWithNotExistingPatient() {
+        final List<JpaRecord> recs =  this.trxManager.execute((final Store store) ->{
+            final PatientRepository rep = store.find(PatientRepository.class);
+            return  rep.getAllRecordsById(INVALID_PIN).stream().map(r -> new JpaRecord(r.getAppointment(),r.getAnamnesis(),r.getObservation(),r.getTherapy())).collect(Collectors.toList());
+        });
 
+        assertEquals(0,recs.size());
+    }
 
 
 }
