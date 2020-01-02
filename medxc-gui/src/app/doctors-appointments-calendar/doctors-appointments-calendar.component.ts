76f91abca781162ba72277ce29d5a2e9a62ebaab
aleksandar.fs@gmail.com <aleksandar.fs@gmail.com>
2020-01-02 15:06:20 +0200

    Integrated in typescript

--- medxc-gui/src/app/doctors-appointments-calendar/doctors-appointments-calendar.component.ts
+++ medxc-gui/src/app/doctors-appointments-calendar/doctors-appointments-calendar.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewChecked, AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FullCalendarComponent} from "@fullcalendar/angular";
 import {MatDialog} from "@angular/material/dialog";
 import {MatDatepickerInputEvent} from "@angular/material/datepicker";
@@ -10,74 +10,85 @@ import { CalendarService } from '../calendar.service';
 import {EventDialogComponent} from "../event-dialog/event-dialog.component";
 import {DatePipe} from "@angular/common";
 import {Event} from '../model/event';
-
+import * as $ from "jquery";
+import {Calendar} from "@fullcalendar/core";
 @Component({
   selector: 'app-doctors-appointments-calendar',
   templateUrl: './doctors-appointments-calendar.component.html',
   styleUrls: ['./doctors-appointments-calendar.component.css']
 })
-export class DoctorsAppointmentsCalendarComponent{
+export class DoctorsAppointmentsCalendarComponent implements AfterViewInit {
 
-  @ViewChild('calendar', { 'static': false })
-  calendarComponent: FullCalendarComponent; // the #calendar in the template*/
+
+ @ViewChild('calendar', { 'static': false })
+ calendarComponent:ElementRef;
+  calendar:Calendar;
   calendarPlugins = [dayGridPlugin, timeGrigPlugin, interactionPlugin];
   calendarVisible = true;
   calendarEvents: Event[] = [
-  ];
+ ];
+  ngAfterViewInit() {
+    console.log("Callsed after view init",this.calendarComponent.nativeElement);
+    this.renderAppointments();
+    this.initCalendar();
+
+    console.log(this.calendarEvents);
+  }
   fromDate = new Date();
   toDate = new Date();
   id : string;
-  constructor(
-    private calendarService: CalendarService,
-    public dialog: MatDialog, private datePipe: DatePipe) {
-  }
+
 
   ngOnInit() {
-     this.renderAppointments();
+
+
+  }
+ constructor(
+   private calendarService: CalendarService,
+   public dialog: MatDialog, private datePipe: DatePipe) {
+
+ }
+ setDate(evt: MatDatepickerInputEvent<Date>) {
+   let moments = moment(evt.value);
+   let calendarApi = this.calendarComponent.nativeElement.getApi();
+  calendarApi.gotoDate(moments.format("YYYY-MM-DD")); // call a method on the Calendar object*/
   }
 
-  setDate(evt: MatDatepickerInputEvent<Date>) {
-    let moments = moment(evt.value);
-    let calendarApi = this.calendarComponent.getApi();
-    calendarApi.gotoDate(moments.format("YYYY-MM-DD")); // call a method on the Calendar object*/
-  }
+  handleDateClick(arg) {
 
-  handleDateClick(arg): void {
-    let now = moment(arg.date);
 
-    let data = { id: "alex", start: arg.date, durations: {}, keys: [] };
+      let now = moment(arg.date);
 
-    data.durations = {
-      '15': now.add(15, "minutes").local().format(),
-      '30': now.add(15, "minutes").local().format(),
-      '45': now.add(15, "minutes").local().format()
-    };
-    data.keys = Object.keys(data.durations);
-    const dialogRef = this.dialog.open(EventDialogComponent, {
-      data,
-      width: '550px',
-    });
+      let data = { id: "alex", start: arg.date, durations: {}, keys: [] };
 
-    dialogRef.afterClosed().subscribe(
-      data => {
-        if (data == undefined) {
-          return;
+      data.durations = {
+        '15': now.add(15, "minutes").local().format(),
+        '30': now.add(15, "minutes").local().format(),
+        '45': now.add(15, "minutes").local().format()
+      };
+      data.keys = Object.keys(data.durations);
+      const dialogRef = this.dialog.open(EventDialogComponent, {
+        data,
+        width: '550px',
+      });
+
+      dialogRef.afterClosed().subscribe(
+        data => {
+          if (data == undefined) {
+            return;
+          }
+          let start = moment(data.start);
+          let end = moment(data.end);
+          this.calendarService.addAppointment(start, end);
         }
-        let start = moment(data.start);
-        let end = moment(data.end);
-        this.calendarService.addAppointment(start, end);
-        this.renderAppointments();
-      }
-    );
+      );
   }
 
   renderAppointments(): void {
     this.calendarService.getEvents().subscribe(data => {
-
       // JOIN the data, this kind of change is needed to trigger rerender of the calendar
       this.calendarEvents = Object.assign([], this.calendarEvents, data);
     });
-
   }
   viewAppointments(id):void{
 
@@ -93,4 +104,31 @@ export class DoctorsAppointmentsCalendarComponent{
   }
 
 
+
+  private initCalendar() {
+      this.calendar = new Calendar(this.calendarComponent.nativeElement,{
+        header: {
+          left   : 'prev,next today',
+          center : 'title',
+          right  : 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        slotDuration : "00:15:00",
+        allDaySlot:false,
+        eventTimeFormat:{ hour: '2-digit',minute: '2-digit',hour12: false},
+        minTime :"07:30:00",
+        maxTime : "19:30:00",
+        defaultView : "timeGridWeek",
+        plugins : this.calendarPlugins,
+        navLinks   : true,
+        editable   : true,
+        eventLimit : true,
+        events : this.calendarEvents,
+        contentHeight :600,
+      });
+
+    this.calendar.render();
+
+
+
+  }
 }
